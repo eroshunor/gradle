8e8dfb5e16711534e2a223dce5092c30722df442
Előd Hampel <elod.hampel@contractors.roche.com>
2026-02-18 18:52:53 +0200

    Removes unused SendMessageUseCase
    Removes the SendMessageUseCase dependency from the HomeViewModel.
    This simplifies the ViewModel as the use case is no longer needed in the current implementation.

--- app/src/main/java/com/example/messagesender/presentation/home_screen/HomeViewModel.kt
+++ app/src/main/java/com/example/messagesender/presentation/home_screen/HomeViewModel.kt
@@ -1,14 +1,12 @@
 package com.example.messagesender.presentation.home_screen
 
 import androidx.lifecycle.ViewModel
-import com.example.messagesender.domain.use_case.SendMessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val sendMessageUseCase: SendMessageUseCase
-): ViewModel() {
+) : ViewModel() {
 
 
 }
